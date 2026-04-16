a5fef7975ab31c39c26f5d28cb85beff232d1a66
Yasmine Berrahal <berrahalyasmine04@gmail.com>
2026-04-16 11:54:13 +0100

    username technicien register

--- ProjetTicketingPfe/projet0.Application/Commun/DTOs/RegisterDTO.cs
+++ ProjetTicketingPfe/projet0.Application/Commun/DTOs/RegisterDTO.cs
@@ -9,7 +9,7 @@ namespace projet0.Application.Commun.DTOs
     public class RegisterDTO
     {
         [Required(ErrorMessage = "Le nom d'utilisateur est requis")]
-        [MinLength(4, ErrorMessage = "Le nom d'utilisateur doit contenir au moins 4 caractères")]
+        [MinLength(3, ErrorMessage = "Le nom d'utilisateur doit contenir au moins 3 caractères")]
         [MaxLength(30, ErrorMessage = "Le nom d'utilisateur ne peut pas dépasser 30 caractères")]
         public string UserName { get; set; }
 
@@ -23,12 +23,12 @@ namespace projet0.Application.Commun.DTOs
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Le nom est requis")]
-        [MinLength(4, ErrorMessage = "Le nom doit contenir au moins 4 caractères")]
+        [MinLength(3, ErrorMessage = "Le nom doit contenir au moins 3 caractères")]
         [MaxLength(30, ErrorMessage = "Le nom ne peut pas dépasser 30 caractères")]
         public string Nom { get; set; }
 
         [Required(ErrorMessage = "Le prénom est requis")]
-        [MinLength(4, ErrorMessage = "Le prénom doit contenir au moins 4 caractères")]
+        [MinLength(3, ErrorMessage = "Le prénom doit contenir au moins 3 caractères")]
         [MaxLength(30, ErrorMessage = "Le prénom ne peut pas dépasser 30 caractères")]
         public string Prenom { get; set; }
 
